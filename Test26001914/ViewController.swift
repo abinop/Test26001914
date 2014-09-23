dbde7f0cff52b7b18b6053455b176d0de3acac66
abinop <tcadmin@TopCreations-MacBook.local>
2014-09-23 23:38:26 +0300

    init

--- Test26001914/ViewController.swift
+++ Test26001914/ViewController.swift
@@ -9,16 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var page_text : UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override  func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func b_hide_keyboard(sender: AnyObject) {
+        self.page_text.resignFirstResponder()
+    }
+    
+    func keyboardWillShow(notification:NSNotification){
+      
+        
+        self.page_text.frame = CGRectMake(self.page_text.frame.origin.x, self.page_text.frame.origin.y, self.page_text.frame.width, (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue().origin.y - 180)
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.page_text.frame = CGRectMake(self.page_text.frame.origin.x, self.page_text.frame.origin.y, self.page_text.frame.width, (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue().origin.y + 180)
+    }
+    
 
 
 }
